6a9921cb29a6eb72171970645b4c33838452ac47
Harvey S <hlsamuel00@yahoo.com>
2022-10-06 14:37:34 -0400

    first commit to Huntober Repo

---
+++ String-Week/challenge-day1.js
@@ -0,0 +1,28 @@
+const mixup = (string) => {
+    const mapObj = {
+        0: 'O',
+        1: 'I',
+        2: 'Z',
+        3: 'E',
+        4: 'h',
+        5: 'S',
+        6: 'G',
+        7: 'L',
+        8: 'B',
+        9: 'q',
+        'O': 0,
+        'I': 1,
+        'Z': 2,
+        'E': 3,
+        'h': 4,
+        'S': 5,
+        'G': 6, 
+        'L': 7,
+        'B': 8,
+        'q': 9
+    }
+    const regex = new RegExp(Object.keys(mapObj).join('|'), 'g')
+    return string.replace(regex, (matched) => mapObj[matched])
+}
+
+console.log(mixup("PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770."))
