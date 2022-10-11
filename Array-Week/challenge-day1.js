9e3ad0f83fec581470f3dbe55d62fe1d4f954839
Harvey S <hlsamuel00@yahoo.com>
2022-10-11 10:28:23 -0400

    changed all solutions to Array prototype methods

--- Array-Week/challenge-day1.js
+++ Array-Week/challenge-day1.js
@@ -1,5 +1,5 @@
-const checkEquality = (arr1, arr2) => {
-    return arr1.map((el,i) => el.map((e,j) => e == arr2[i][j]).every(Boolean)).every(Boolean)
+Array.prototype.checkEquality = function(arr2){
+    return this.map((el,i) => el.map((e,j) => e == arr2[i][j]).every(Boolean)).every(Boolean)
 }
 
 const array1 = [ ['a','b','c'], ['d','e','f'], ['g','h','i'] ]
@@ -7,6 +7,7 @@ const array2 = [ ['a','b','c'], ['d','e','f'], ['g','h','i'] ]
 const array3 = [ ['a','B','c'], ['d','E','f'], ['g','H','i'] ]
 const array4 = [ ['a','b','c'], ['g','h','i'], ['d','e','f'] ]
 
-console.log(checkEquality(array1, array2))
-console.log(checkEquality(array1, array3))
-console.log(checkEquality(array1, array4))
+console.log(array1.checkEquality(array2))
+console.log(array1.checkEquality(array3))
+console.log(array1.checkEquality(array4))
+console.log(array1.checkEquality(array1))
